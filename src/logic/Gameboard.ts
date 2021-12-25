dfdf6d7328b8bd0d40786fb180f640b776fc5928
cineonizer <jamesdkiam@gmail.com>
2021-12-25 11:40:37 -0800

    added some logic for placing ship

--- src/logic/Gameboard.ts
+++ src/logic/Gameboard.ts
@@ -2,22 +2,36 @@ import Ship from './Ship';
 
 interface GameboardInterface {
   size: number;
-  board: Ship[][];
+  board: (Ship | null)[][];
+  initializeBoard(): null[][];
+  placeShip(ship: Ship, row: number, col: number): void;
 }
 
 class Gameboard implements GameboardInterface {
   readonly size: number;
-  board: Ship[][];
+  board: (Ship | null)[][];
 
   constructor() {
     this.size = 10;
-    this.board = this.initializeBoard();
+    this.board = this.initializeBoard() as null[][];
   }
 
   initializeBoard() {
     return Array(this.size)
       .fill(null)
       .map(() => Array(this.size).fill(null));
+  }
+
+  placeShip(ship: Ship, row: number, col: number) {
+    if (!this.isPlacementValid) return;
+
+    for (let i = 0; i < ship.length; i++) {
+      this.board[row][col + i] = ship;
+    }
+  }
+
+  private isPlacementValid(): boolean {
+    return true;
   }
 }
 
--- src/logic/tests/Gameboard.test.ts
+++ src/logic/tests/Gameboard.test.ts
@@ -1,9 +1,28 @@
 import Gameboard from '../Gameboard';
+import Ship from '../Ship';
 
 describe('The gameboard', () => {
+  let game: Gameboard;
+  let testBoard: (Ship | null)[][]
+  let carrier: Ship;
+  let battleship: Ship;
+  let cruiser: Ship;
+  let submarine: Ship;
+  let destroyer: Ship;
+
+  beforeEach(() => {
+    game = new Gameboard();
+    testBoard = Array(10).fill(null).map(() => Array(10).fill(null))
+    carrier = new Ship(5);
+    battleship = new Ship(4);
+    cruiser = new Ship(3);
+    submarine = new Ship(3);
+    destroyer = new Ship(2);
+  });
+
   test('is created and initialized', () => {
-    const gameboard = new Gameboard();
-    expect(gameboard.board).toEqual([
+    // verify that the test board has the correct number of rows and columns in 2D array
+    expect(testBoard).toEqual([
       [null, null, null, null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null, null, null, null],
@@ -15,5 +34,17 @@ describe('The gameboard', () => {
       [null, null, null, null, null, null, null, null, null, null],
       [null, null, null, null, null, null, null, null, null, null],
     ]);
+    // then test the game board against the test board, which has the same code
+    expect(game.board).toEqual(testBoard);
+  });
+
+  test('places a ship horizontally', () => {
+    game.placeShip(carrier, 4, 5);
+    testBoard[4][5] = carrier
+    testBoard[4][6] = carrier
+    testBoard[4][7] = carrier
+    testBoard[4][8] = carrier
+    testBoard[4][9] = carrier
+    expect(game.board).toEqual(testBoard);
   });
 });
